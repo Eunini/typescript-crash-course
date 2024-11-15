6e0d62bc7a300c2ce1adae9433e2c6e990ec65a4
eunice <atandainioluwa371@gmail.com>
2024-11-14 23:34:11 -0800

    latest

---
+++ dist/classes/Payment.js
@@ -0,0 +1,11 @@
+// classes
+export class Payment {
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
+    }
+    format() {
+        return `${this.client} is owed £${this.amount} for ${this.details}`;
+    }
+}
